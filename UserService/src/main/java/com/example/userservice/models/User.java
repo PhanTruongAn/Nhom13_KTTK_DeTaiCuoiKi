6dc41a1aebc054b9ff82e1bfc5b5eb7a3c7550a7
PhanTruongAn <phanan1000@gmail.com>
2024-05-10 07:27:14 +0700

    ok

--- UserService/src/main/java/com/example/userservice/models/User.java
+++ UserService/src/main/java/com/example/userservice/models/User.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
@@ -17,4 +20,6 @@ public class User {
     private String userName;
     private String password;
     private String email;
+    @ManyToOne
+    private List<Novel> theoDoi;
 }
